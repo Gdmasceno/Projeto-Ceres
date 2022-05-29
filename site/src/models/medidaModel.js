5a548248dedd54429a92d6da5edb265b1b303d4c
Gdmasceno <gabriel.doliveira@sptech.school>
2022-05-29 20:19:32 -0300

    atualizando a main

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,20 +1,29 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idPlantacao, limite_linhas) {
+    // console.log("AAAAAAAAAAAAAAAAAA");
 
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        umidade, temperatura, dtCaptura,
+        instrucaoSql = `
+        select top ${limite_linhas}
+        temperatura as temperatura, 
+        umidade as umidade,  
+                        dtCaptura,
                         CONVERT(varchar, dtCaptura, 108) as momento_grafico
-                    from dadosdht11 join sensor on dadosdht11.fkSensor = sensor.idSensor
+                    from dadosdht11 join sensor on idSensor = fkSensor
                     where sensor.fkPlantacao = ${idPlantacao}
-                    order by sensor.fkPlantacao desc`;
-
+                    order by idDadosDHT11 desc;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select umidade, temperatura, dtCaptura from dadosdht11 join sensor on dadosdht11.fkSensor = sensor.idSensor
-        where sensor.fkPlantacao = ${idPlantacao} order by dtCaptura desc;`;
+        instrucaoSql = `select 
+        temperatura as temperatura, 
+        umidade as umidade,
+                        dtCaptura,
+                        DATE_FORMAT(dtCaptura,'%H:%i:%s') as momento_grafico
+                    from dadosdht11 join sensor on idSensor = fkSensor
+                    where sensor.fkPlantacao = ${idPlantacao}
+                    order by idDadosDHT11 desc limit ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -29,18 +38,22 @@ function buscarMedidasEmTempoReal(idPlantacao) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select umidade, temperatura, dtCaptura, 
-                        CONVERT(varchar, dtCaptura, 108) as momento_grafico , sensor.fkPlantacao
-                        from dadosdht11 join sensor on dadosdht11.fkSensor = sensor.idSensor
-                        where sensor.fkPlantacao = ${idPlantacao} order by sensor.fkPlantacao desc`;
+        instrucaoSql = `select top 1
+        temperatura as temperatura, 
+        umidade as umidade,  
+                        CONVERT(varchar, dtCaptura, 108) as momento_grafico, 
+                        sensor.fkPlantacao 
+                        from dadosdht11 join sensor on idSensor = fkSensor where sensor.fkPlantacao = ${idPlantacao} 
+                    order by idDadosDHT11 desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-        umidade, temperatura,
-                        DATE_FORMAT(dadosdht11.dtCaptura,'%H:%i:%s') as momento_grafico,
-                        sensor.fkPlantacao
-                        from dadosdht11 join sensor on dadosdht11.fkSensor = sensor.idSensor 
-                        where sensor.fkPlantacao = ${idPlantacao} order by sensor.fkPlantacao desc`;
+        temperatura as temperatura, 
+        umidade as umidade,
+                        DATE_FORMAT(dtCaptura,'%H:%i:%s') as momento_grafico, 
+                        sensor.fkPlantacao 
+                        from dadosdht11 join sensor on idSensor = fkSensor where sensor.fkPlantacao = ${idPlantacao} 
+                    order by idDadosDHT11 desc limit 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
